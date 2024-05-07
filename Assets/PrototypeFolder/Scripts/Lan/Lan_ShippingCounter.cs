c2fb10155434f137bb4253d4e3463cc9ed7c30bd
HoangLanlam <hoanglan8140304@gmail.com>
2024-05-07 20:50:29 +0700

    Successfully deliver the food but the customer still haven't get the food

--- Assets/PrototypeFolder/Scripts/Lan/Lan_ShippingCounter.cs
+++ Assets/PrototypeFolder/Scripts/Lan/Lan_ShippingCounter.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lan_ShippingCounter : MonoBehaviour
+public class ShippingCounter : MonoBehaviour
 {
-    List<string> currentOrder = new List<string>(); // Temporarily holds the food items placed on the counter
+    public Transform raycastOrigin; // The origin point of the raycast
+    public float raycastLength = 2f; // Length of the raycast
+    public LayerMask foodLayer; // Layer that only food items are on
+    private List<string> currentOrder = new List<string>(); // Temporarily holds the food items placed on the counter
 
-    void OnTriggerEnter2D(Collider2D other)
+    void Update()
     {
-        if (other.tag == "FoodItem")
+        // Cast a ray forward from the raycast origin
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, Vector2.right, raycastLength, foodLayer);
+        if (hit.collider != null && hit.collider.CompareTag("Lan_FoodItem"))
         {
-            currentOrder.Add(other.gameObject.name);
-            Destroy(other.gameObject); // Optionally destroy the food item game object
-            CheckOrders();
+            ProcessFoodItem(hit.collider);
+            Debug.Log(hit.transform.name +" have been delivered");
         }
+    }
+
+    void ProcessFoodItem(Collider2D item)
+    {
+        // Process the food item that was hit by the ray
+        currentOrder.Add(item.gameObject.name);
+        Destroy(item.gameObject); // Remove the food item from the scene
+        CheckOrders();
     }
 
     void CheckOrders()
@@ -23,15 +35,25 @@ public class Lan_ShippingCounter : MonoBehaviour
             if (!customer.isBeingServed && HasMatchingOrder(customer.requestedFoods, currentOrder))
             {
                 customer.ServeCustomer();
-                currentOrder.Clear(); 
-                break; 
+                currentOrder.Clear(); // Clear the order list after serving
+                break; // Exit the loop after serving one customer
             }
         }
     }
 
     bool HasMatchingOrder(List<string> requestedFoods, List<string> deliveredFoods)
     {
-      
+        // Check if all requested foods are in the delivered foods
         return new HashSet<string>(requestedFoods).SetEquals(deliveredFoods);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw the ray in the editor to visualize it
+        if (raycastOrigin != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(raycastOrigin.position, raycastOrigin.position + Vector3.right * raycastLength);
+        }
     }
 }
